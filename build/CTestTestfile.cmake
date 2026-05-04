dc747980ed4903c9fc746a73db37f5ebb17bfd73
AntoanNikolov <antoanameya@gmail.com>
2026-05-04 19:59:12 -0400

    Filepath fix

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Antoan/Downloads/LoopFinder
-# Build directory: C:/Users/Antoan/Downloads/LoopFinder/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("JUCE")
-subdirs("Tests")
